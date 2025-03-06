ca486e51f83041003f4ac859732b4931af3790f4
ray-672 <Saskiyadesilva@gmail.com>
2025-03-06 18:45:27 +0530

    Add files via upload

---
+++ changing data types.py
@@ -0,0 +1,16 @@
+name = 'raine'
+age = 13
+weight = 43.6
+student = 'False'
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(student))
+
+age = str(age)
+print(type(age))
+print(age)
+
+weight = int(weight)
+print(weight)
+print(type(weight))
---
+++ data types.py
@@ -0,0 +1,8 @@
+a = 12
+b = 'hello'
+c = 36.72
+d = True
+print('a data type is... ',type(a)) 
+print('b data type is... ',type(b))
+print('c data type is... ',type(c))
+print('d data type is... ',type(d))
